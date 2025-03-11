0c676aa7fb5360a40da5cba2619f8e3293db5ea4
Timeless1696699 <Timeless1696699@outlook.com>
2025-03-11 13:15:40 -0400

    Fixing width and height of Canvas

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-
+var screenWidth = screen.width;
+var screenHeight = screen.height;
 
 function preload(){
    
 }
 
 function setup(){
-    createCanvas(width, height,Canvas1);
+    createCanvas(screenWidth, screenHeight, Canvas1);
 }
 function draw(){
     background("blue");
